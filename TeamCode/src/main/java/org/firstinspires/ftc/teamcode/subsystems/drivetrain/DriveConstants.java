730d1427966be18ca9a4837fbea0c3f105cfa348
PandaKewt <chuminhtuan30306@gmail.com>
2025-02-13 15:08:54 +0700

    done tuning rr. start build path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/DriveConstants.java
@@ -54,8 +54,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.022;
-    public static double kA = 0.002;
+    public static double kV = 0.023;
+    public static double kA = 0.003;
     public static double kStatic = 0.0;
 
     /*
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(177.1868877804112);
+    public static double MAX_ANG_VEL = Math.toRadians(177.47297161983502);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
